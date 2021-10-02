c6b403d04e3709616dd56978cb04dd461789eb3c
Brian Thiely <brian.thiely@gmail.com>
2021-10-02 10:40:45 +0200

    add config multer

---
+++ middleware/multer-config.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+	'image/jpg': 'jpg',
+	'image/jpeg': 'jpg',
+	'image/png': 'png',
+};
+// Indique à Multer la logique de stockage des fichiers entrants
+const storage = multer.diskStorage({
+    // Func dest indique d'enregistrer les photos dans le dossier images
+	destination: (req, file, callback) => {
+		callback(null, 'images');
+	},
+    // Func filename indique indique d'utiliser le noms d'origine, remplacer les espace par underscore et ajouter extension appropriers
+	filename: (req, file, callback) => {
+		const name = file.originalname.split(' ').join('_');
+		const extension = MIME_TYPES(file.mimetype);
+		callback(null, name + Date.now() + '.' + extension);
+	},
+});
+
+module.exports = multer({ storage }).single('image');
