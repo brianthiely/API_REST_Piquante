91cae0babfd52f5a1b1dbaff92734526b0d4a514
Brian Thiely <brian.thiely@gmail.com>
2021-10-02 16:44:42 +0200

    add like and dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -9,7 +9,7 @@ exports.createSauce = (req, res, next) => {
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .then(() => res.status(201).json({ message: 'Sauce ajouté !'}))
     .catch(error => res.status(400).json({ error }));
 };
 
@@ -41,7 +41,7 @@ exports.modifySauce = (req, res, next) => {
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
             Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+            .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
             .catch(error => res.status(400).json({ error }));
         })
     })
@@ -53,7 +53,7 @@ exports.deleteSauce = (req, res, next) => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
           .catch(error => res.status(400).json({ error }));
       });
     })
@@ -73,3 +73,48 @@ exports.getAllSauce = (req, res, next) => {
     }
   );
 };
+
+exports.likeSauce = (req, res, next) => {
+    const userId = req.body.userId;
+    const like = req.body.like;
+    const sauceId = req.params.id;
+
+    Sauce.findOne({ _id: sauceId })
+        .then(sauce => {
+            // nouvelles valeurs à modifier
+            const newValues = {
+                usersLiked: sauce.usersLiked,
+                usersDisliked: sauce.usersDisliked,
+                likes: 0,
+                dislikes: 0
+            }
+            // Différents cas:
+            switch (like) {
+                case 1:  // CAS: sauce liked
+                    newValues.usersLiked.push(userId);
+                    break;
+                case -1:  // CAS: sauce disliked
+                    newValues.usersDisliked.push(userId);
+                    break;
+                case 0:  // CAS: Annulation du like/dislike
+                    if (newValues.usersLiked.includes(userId)) {
+                        // si on annule le like
+                        const index = newValues.usersLiked.indexOf(userId);
+                        newValues.usersLiked.splice(index, 1);
+                    } else {
+                        // si on annule le dislike
+                        const index = newValues.usersDisliked.indexOf(userId);
+                        newValues.usersDisliked.splice(index, 1);
+                    }
+                    break;
+            };
+            // Calcul du nombre de likes / dislikes
+            newValues.likes = newValues.usersLiked.length;
+            newValues.dislikes = newValues.usersDisliked.length;
+            // Mise à jour de la sauce avec les nouvelles valeurs
+            Sauce.updateOne({ _id: sauceId }, newValues )
+                .then(() => res.status(200).json({ message: 'Sauce notée !' }))
+                .catch(error => res.status(400).json({ error }))  
+        })
+        .catch(error => res.status(500).json({ error }));
+}
