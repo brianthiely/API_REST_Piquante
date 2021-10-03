821f6d0ca88c55b8e213397bf1a412627bbab00d
Brian Thiely <brian.thiely@gmail.com>
2021-10-03 13:22:16 +0200

    refact ctrl sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -36,20 +36,15 @@ exports.getOneSauce = (req, res, next) => {
 // Logique exporter dans les routes pour la modification d'une sauce
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
+  
     {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      
     } : { ...req.body };
-
-    Sauce.findOne({_id: req.params.id})
-    .then(sauce => {
-        const filename = sauce.imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`, () => {
-            Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
-            .catch(error => res.status(400).json({ error }));
-        })
-    })
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 };
 
 // Logique exporter dans les routes pour la supression d'une sauce
