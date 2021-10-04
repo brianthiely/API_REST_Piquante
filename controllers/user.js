7c80bb5bb417f78a98c8a736324a86b5658ed553
Brian Thiely <brian.thiely@gmail.com>
2021-10-04 14:22:29 +0200

    change token secret

--- controllers/user.js
+++ controllers/user.js
@@ -34,7 +34,7 @@ exports.login = (req, res, next) => {
 					}
 					res.status(200).json({
 						userId: user._id,
-						token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+						token: jwt.sign({ userId: user._id }, 'kgIGIYTG87669GGJF', {
 							expiresIn: '24h',
 						}),
 					});
